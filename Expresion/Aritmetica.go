a502f885f19c2f4a519044d6724a79183e0900b9
José Francisco de Jesús Santos Salazar <josejfss98@gmail.com>
2022-02-15 23:24:02 -0600

    Multiplicacion y Resta

--- Expresion/Aritmetica.go
+++ Expresion/Aritmetica.go
@@ -39,60 +39,142 @@ func (p Aritmetica) Interpretar(tree TS.Arbol, table TS.TablaSimbolo) interfaces
 			retorno.Fila = p.fila
 			resultado := izq.Valor.(int) + der.Valor.(int)
 			retorno.Valor = resultado
+			return retorno
 		} else if izq.Tipo == TS.ENTERO && der.Tipo == TS.DECIMAL {
 			retorno.Tipo = TS.DECIMAL
 			retorno.Columna = p.columna
 			retorno.Fila = p.fila
 			resultado := float64(izq.Valor.(int)) + der.Valor.(float64)
 			retorno.Valor = resultado
+			return retorno
 		}else if izq.Tipo == TS.ENTERO && der.Tipo == TS.CADENA {
 			retorno.Tipo = TS.CADENA
 			retorno.Columna = p.columna
 			retorno.Fila = p.fila
 			resultado := strconv.Itoa(izq.Valor.(int)) + der.Valor.(string)
 			retorno.Valor = resultado
+			return retorno
 		} else if izq.Tipo == TS.CADENA && der.Tipo == TS.ENTERO {
 			retorno.Tipo = TS.CADENA
 			retorno.Columna = p.columna
 			retorno.Fila = p.fila
 			resultado := izq.Valor.(string) + strconv.Itoa(der.Valor.(int))
 			retorno.Valor = resultado
+			return retorno
 		}else if izq.Tipo == TS.DECIMAL && der.Tipo == TS.CADENA {
 			retorno.Tipo = TS.CADENA
 			retorno.Columna = p.columna
 			retorno.Fila = p.fila
 			resultado := fmt.Sprintf("%.2f", izq.Valor.(float64)) + der.Valor.(string)
 			retorno.Valor = resultado
+			return retorno
 		} else if izq.Tipo == TS.CADENA && der.Tipo == TS.DECIMAL {
 			retorno.Tipo = TS.CADENA
 			retorno.Columna = p.columna
 			retorno.Fila = p.fila
 			resultado := izq.Valor.(string) + fmt.Sprintf("%.2f",der.Valor.(float64))
 			retorno.Valor = resultado
+			return retorno
 		} else if izq.Tipo == TS.CADENA && der.Tipo == TS.CADENA {
 			retorno.Tipo = TS.CADENA
 			retorno.Columna = p.columna
 			retorno.Fila = p.fila
 			resultado := izq.Valor.(string) + der.Valor.(string)
 			retorno.Valor = resultado
+			return retorno
 		} else if izq.Tipo == TS.DECIMAL && der.Tipo == TS.ENTERO {
 			retorno.Tipo = TS.DECIMAL
 			retorno.Columna = p.columna
 			retorno.Fila = p.fila
 			resultado := izq.Valor.(float64) + float64(der.Valor.(int))
 			retorno.Valor = resultado
+			return retorno
 		}else if izq.Tipo == TS.DECIMAL && der.Tipo == TS.DECIMAL {
 			retorno.Tipo = TS.DECIMAL
 			retorno.Columna = p.columna
 			retorno.Fila = p.fila
 			resultado := izq.Valor.(float64) + der.Valor.(float64)
 			retorno.Valor = resultado
+			return retorno
 		}
 
 		//error en suma
 		retorno.Tipo = TS.ERROR
 		retorno.Valor = TS.NuevaExcepcion("Semantico", "Operacion de suma invalido", p.fila, p.columna)
 		return retorno
+	//RESTA
+	}else if p.operador == TS.MENOS {
+		if izq.Tipo == TS.ENTERO && der.Tipo == TS.ENTERO {
+			retorno.Tipo = TS.ENTERO
+			retorno.Columna = p.columna
+			retorno.Fila = p.fila
+			resultado := izq.Valor.(int) - der.Valor.(int)
+			retorno.Valor = resultado
+			return retorno
+		} else if izq.Tipo == TS.ENTERO && der.Tipo == TS.DECIMAL {
+			retorno.Tipo = TS.DECIMAL
+			retorno.Columna = p.columna
+			retorno.Fila = p.fila
+			resultado := float64(izq.Valor.(int)) - der.Valor.(float64)
+			retorno.Valor = resultado
+			return retorno
+		} else if izq.Tipo == TS.DECIMAL && der.Tipo == TS.ENTERO {
+			retorno.Tipo = TS.DECIMAL
+			retorno.Columna = p.columna
+			retorno.Fila = p.fila
+			resultado := izq.Valor.(float64) - float64(der.Valor.(int))
+			retorno.Valor = resultado
+			return retorno
+		}else if izq.Tipo == TS.DECIMAL && der.Tipo == TS.DECIMAL {
+			retorno.Tipo = TS.DECIMAL
+			retorno.Columna = p.columna
+			retorno.Fila = p.fila
+			resultado := izq.Valor.(float64) - der.Valor.(float64)
+			retorno.Valor = resultado
+			return retorno
+		}
+
+		//error en suma
+		retorno.Tipo = TS.ERROR
+		retorno.Valor = TS.NuevaExcepcion("Semantico", "Operacion de resta invalido", p.fila, p.columna)
+		return retorno
+	//MULTIPLICACION
+	}else if p.operador == TS.POR {
+		if izq.Tipo == TS.ENTERO && der.Tipo == TS.ENTERO {
+			retorno.Tipo = TS.ENTERO
+			retorno.Columna = p.columna
+			retorno.Fila = p.fila
+			resultado := izq.Valor.(int) * der.Valor.(int)
+			retorno.Valor = resultado
+			return retorno
+		} else if izq.Tipo == TS.ENTERO && der.Tipo == TS.DECIMAL {
+			retorno.Tipo = TS.DECIMAL
+			retorno.Columna = p.columna
+			retorno.Fila = p.fila
+			resultado := float64(izq.Valor.(int)) * der.Valor.(float64)
+			retorno.Valor = resultado
+			return retorno
+		} else if izq.Tipo == TS.DECIMAL && der.Tipo == TS.ENTERO {
+			retorno.Tipo = TS.DECIMAL
+			retorno.Columna = p.columna
+			retorno.Fila = p.fila
+			resultado := izq.Valor.(float64) * float64(der.Valor.(int))
+			retorno.Valor = resultado
+			return retorno
+		}else if izq.Tipo == TS.DECIMAL && der.Tipo == TS.DECIMAL {
+			retorno.Tipo = TS.DECIMAL
+			retorno.Columna = p.columna
+			retorno.Fila = p.fila
+			resultado := izq.Valor.(float64) * der.Valor.(float64)
+			retorno.Valor = resultado
+			return retorno
+		}
+
+		//error en suma
+		retorno.Tipo = TS.ERROR
+		retorno.Valor = TS.NuevaExcepcion("Semantico", "Operacion de multiplicacion invalido", p.fila, p.columna)
+		return retorno
+	//MULTIPLICACION
 	}
 
 	return retorno
